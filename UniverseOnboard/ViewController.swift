095028066d4169c4087101aed3f73f60a0aa492d
n1kitka <98713485+n1kitka@users.noreply.github.com>
2024-11-29 14:21:09 +0200

    Initial Commit

---
+++ UniverseOnboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniverseOnboard
+//
+//  Created by Никита Савенко on 29.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
